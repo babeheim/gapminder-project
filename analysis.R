9dce0e0fe81c0f395de626debd53c397df01f2d4
Bret Beheim <beheim@gmail.com>
2019-12-10 11:03:55 +0100

    an unfortunate commit that breaks some code

--- analysis.R
+++ analysis.R
@@ -5,4 +5,4 @@
 
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv", destfile = "data/gapminder_data.csv")
 
-gapminder <- read.csv("data/gapminder_data.csv")
+gapminder <- read.csv("data/gapminder_data.csvv")
